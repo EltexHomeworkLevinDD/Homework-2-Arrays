61c9e445ccbf5c1035b6a386e4aa303b84fac298
EltexHomeworkLevinDD <pykanus172@gmail.com>
2023-11-06 20:21:59 +0700

    work + screenshots

--- main.c
+++ main.c
@@ -1,13 +1,31 @@
 /*
-Branch task-1
-    Вывести квадратную матрицу по заданному N
-
 Branch task-2
     Вывести заданный массив размером N в обратном порядке
-
-Branch task-3
-    Заполнить верхний треугольник матрицы 1, а нижний 0
-
-Branch task-4
-    Заполнить матрицу числами от 1 до N^2 улиткой
 */
+
+#include <stdio.h>
+
+#define N 11
+
+int main(){
+    int array[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+    printf("Input array:  ");  
+    for (int i = 0; i < N; i++)
+        printf("%-2d ", array[i]);
+
+    int temp;
+    for (int i = 0; i < N / 2; i++)
+    {
+        temp = array[i];
+        array[i] = array[N - 1 - i];
+        array[N - 1 - i] = temp;
+    }
+
+    printf("\nOutput array: ");  
+    for (int i = 0; i < N; i++)
+        printf("%-2d ", array[i]);
+
+    printf("\n\n");
+    return 0;
+}
